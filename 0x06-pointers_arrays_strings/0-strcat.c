6bfeb756b6a3e809c24c3d911bc4e3231a265827
Aghedo.joel <aghedo.joel15@gmail.com>
2022-12-20 23:47:39 -0800

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strcat - concat 2 string
+* @dest:char
+* @src:char
+* Return: 0
+*/
+char *_strcat(char *dest, char *src)
+{
+char *s = dest;
+while (*dest != '\0')
+{
+	dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (s);
+}
